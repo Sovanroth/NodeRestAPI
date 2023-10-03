7dd7c22e927ebf5a8bdfd1253e8665387eb5f833
Sovanroth13 <sovanroth016@gmail.com>
2023-10-03 16:54:16 +0700

    update

--- src/controller/authenication.ts
+++ src/controller/authenication.ts
@@ -2,6 +2,45 @@ import express from "express";
 import { createUser, getUserByEmail } from "../db/users";
 import { random, authenication } from "../helper";
 
+// export const login = async (req: express.Request, res: express.Response) => {
+//   try {
+//     const { email, password } = req.body;
+
+//     if (!email || !password) {
+//       return res.sendStatus(400);
+//     }
+
+//     const user = await getUserByEmail(email).select(
+//       "+authenication.salt +authenication.password"
+//     );
+
+//     if (!user) {
+//       return res.sendStatus(400);
+//     }
+
+//     const expectedHash = authenication(user.authenication.salt, password);
+
+//     if (user.authenication.password != expectedHash) {
+//       return res.sendStatus(403);
+//     } 
+
+//     const salt = random();
+//     user.authenication.sessionToken = authenication(salt, user._id.toString());
+
+//     await user.save();
+
+//     res.cookie("ANTONIO-AUTH", user.authenication.sessionToken, {
+//       domain: "localhost",
+//       path: "/",
+//     });
+
+//     return res.status(200).json(user).end();
+//   } catch (error) {
+//     console.log(error);
+//     return res.sendStatus(400);
+//   }
+// };
+
 export const register = async (req: express.Request, res: express.Response) => {
   try {
     const { email, password, username } = req.body;
